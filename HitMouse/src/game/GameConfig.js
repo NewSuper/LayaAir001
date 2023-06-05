a56ccbef10f49b497e7863fa4d6074e2ae723bc2
NewSuper <newsuper6892@sina.com>
2023-06-05 18:16:58 +0300

    clear

--- HitMouse/src/game/GameConfig.js
+++
@@ -1,9 +0,0 @@
-var GameConfig = {
-    arrPosMouse :[
-      {x: -245,y:0},{x: 0,y:0},{x: 245,y:0}, 
-      {x: -245,y:126},{x: 0,y:126},{x: 245,y:126}, 
-      {x: -245,y:268},{x: 0,y:268},{x: 245,y:268}, 
-    ],
- }
- 
- export default GameConfig;
